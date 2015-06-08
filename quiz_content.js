9ed346c52b22071361addfd8070cf63d6995d7aa
Kerry <kerry.sheldon@gmail.com>
2015-06-07 18:00:08 -0700

    started html and js files and update my design doc.

---
+++ quiz_content.js
@@ -0,0 +1,23 @@
+// THis is my JS file for my project
+// First thing I'm going to do is create question objects
+
+function Question(question, choices, correctAnswer) {
+	this.question = question;
+	this.choices = choices;
+	this.correctAnswer = correctAnswer;
+}
+
+var ques1("How many dogs do I have?", [0, 1, 2, 3], 1);
+var ques2("How old amy I?", [20, 52, 36. 43], 2);
+var ques3("What is my name?", ["Spot", "Finn", "George", "Kerry"], 3);
+
+var allQuestions = [ques1, ques2, ques3];
+
+score = 0
+var total = function(userInputTBD, this.correctAnswer) {
+	if(userInputTBD === this.correctAnswer) {
+		var score= score + 1;}
+	else {
+		var score = score;}
+	return score 
+} 
